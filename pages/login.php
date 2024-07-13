95a5c177a40d4574d364912a6f7463c0cc017548
Ramelito Martinez <ramelitomartinez@gmail.com>
2024-07-13 21:39:15 +0800

    added login page

--- pages/login.php
+++ pages/login.php
@@ -1,5 +1,31 @@
+<link rel="stylesheet" type="text/css" href="../css/pages/auth/login.css" />
 <section class="login">
     <div>
-        
+        <img src="../images/almost-there.gif" />
     </div>
+    <div>
+        <form action="" method="post">
+            <div>
+                <label for="email">Email:</label>
+                <input type="email" id="email" name="email" required />
+            </div>
+
+            <div>
+                <label for="password">Password:</label>
+                <input type="password" id="password" name="password" required>
+            </div>
+
+            <div>
+                <button type="submit">Login</button>
+            </div>
+        </form>
+        <div>
+            <span>Don't have an account?</span>
+            <span><a href="">Sign up</a></span>
+        </div>
+        <div>
+            <span><a href="">Forgot password</a></span>
+        </div>
+    </div>
+
 </section>
